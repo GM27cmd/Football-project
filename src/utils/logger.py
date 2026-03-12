af852b50c83ccafeed972754a15cda62b5388625
GM27cmd <georgimanolovsky27@itpg-varna.bg>
2026-03-12 18:55:22 +0200

    Update logger.py

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,12 +1,8 @@
-from datetime import datetime
+import datetime
 
 LOG_FILE = "commands.log"
 
 
-def log_command(user_input, intent, response):
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    log_line = f"{timestamp} | {user_input} | {intent} | {response}\n"
-
-    with open(LOG_FILE, "a", encoding="utf-8") as file:
-        file.write(log_line)
+def log_command(user_input, intent, result):
+    with open(LOG_FILE, "a", encoding="utf-8") as f:
+        f.write(f"{datetime.datetime.now()} | INPUT: {user_input} | INTENT: {intent} | RESULT: {result}\n")
